2435db9807ffac1093236095c1dc763643fcb0e6
CptTapo4ek <andreib@nextlogic.ro>
2024-07-03 19:01:27 +0300

    fixed project configuration

--- android/app/src/main/kotlin/com/example/document_text_recognition/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/document_text_recognition/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.document_text_recognition
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
